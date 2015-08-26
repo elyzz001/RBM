f7e4a4bc370d63a669404fbd2bcb056afd3131bd
ceroytres <ceroytres>
2015-08-25 22:48:28 -0700

    minor updates

--- binary_RBM.py
+++ binary_RBM.py
@@ -1,18 +1,23 @@
 import numpy as np
 
-
-
 class binary_RBM(object):
 
-	def __init__(self,n_visible=None,n_hidden=256,miniBatch=100,L2_decay=None):
+	def __init__(self,n_visible=None,n_hidden=256,miniBatch=100,L2_decay=None,k=1):
 		self.n_hidden=n_hidden
 		self.n_visible=n_visible
 		self.miniBatch=miniBatch
 		self.L2_decay=L2_decay
-		
+		self.k=k
 		self.W=np.random.randn(n_hidden,n_visible)
 		self.hbias=np.zeros((n_hidden,1))
 		self.vbias=np.zeros((n_visible,1))
 		
-	def fit(self,data):
+	def fit(self,x):
 		
+    def sigmoid(z):
+		return 1/(1+np.exp(-z))
+	
+	def _positivePhase(self,x):
+		p=self.sigmoid(self.W.dot(x.T)+self.hbias);
+		
+		return hidden_states
