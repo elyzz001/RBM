bf612e5752694e91bba3bf532b339c4bfbd49e9c
ceroytres <dmarez8ece@gmail.com>
2016-03-24 15:05:32 -0700

    bug fixes in rbm

--- binary_RBM.py
+++ binary_RBM.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 from numba import jit
 
@@ -15,59 +16,57 @@ class binary_RBM(object):
         self.epoches=epoches
         self.lr=lr
         self.mu=mu
-    
-   # @jit    
+
+    @jit    
     def fit(self,x):
 
-        N=x.shape[0]
-        batches,num_batches=self._batchLists(N)
-
-        num_batches=int(num_batches)
-        
-        
         v_W=np.zeros(self.W.shape)
         v_h=np.zeros(self.hbias.shape)
         v_v=np.zeros(self.vbias.shape)        
-        
+    
         cost=self.get_pseudo_likelihood(x)
-        print "Epoch %d Pseudo-likelihood cost:%f" % (0,cost)
-        
+        print("Epoch %d Pseudo-likelihood cost:%f" % (0,cost))
+    
         for t in range(0,self.epoches):
-
+            N=x.shape[0]
+            batches,num_batches=self._batchLists(N)
+            num_batches=int(num_batches)
             for i in range(0,num_batches):
-
+    
                 idx=batches[i]
                 data=np.squeeze(x[idx,:])
                 B=data.shape[0]
-
+        
                 p_h=self._sigmoid(np.dot(data,self.W)+self.hbias)
                 if t==0 and i==0:
                     h=p_h>np.random.rand(p_h.shape[0],p_h.shape[1])
-                p_v=self._sigmoid(np.dot(h,self.W.T)+self.vbias)
-                v=p_v>np.random.rand(p_v.shape[0],p_v.shape[1])
-
-                q_h=self._sigmoid(np.dot(v,self.W)+self.hbias)
-                h=q_h>np.random.rand(q_h.shape[0],q_h.shape[1])
                 
+                for k in range(0,15):
+                    p_v=self._sigmoid(np.dot(h,self.W.T)+self.vbias)
+                    v=p_v>np.random.rand(p_v.shape[0],p_v.shape[1])
+        
+                    q_h=self._sigmoid(np.dot(v,self.W)+self.hbias)
+                    h=q_h>np.random.rand(q_h.shape[0],q_h.shape[1])
                 
+        
                 g_W=np.dot(data.T,p_h)-np.dot(v.T,q_h)
                 g_W/=B
                 g_v=data.mean(axis=0)-v.mean(axis=0)
                 g_h=p_h.mean(axis=0)-q_h.mean(axis=0)
-                            
-                
+        
+        
                 v_W=self.mu*v_W+self.lr*(g_W-self.alpha*self.W)
                 v_h=self.mu*v_h+self.lr*g_h
                 v_v=self.mu*v_v+self.lr*g_v
-                    
+        
                 self.W+=v_W
                 self.hbias+=v_h
                 self.vbias+=v_v
-                
-                
+    
+    
             self.lr/=(t+2)
             cost=self.get_pseudo_likelihood(x)
-            print "Epoch %d Pseudo-likelihood cost:%f" % (t+1,cost)
+            print("Epoch %d Pseudo-likelihood cost:%f" % (t+1,cost))
         return None
 
 
@@ -78,36 +77,42 @@ class binary_RBM(object):
         batch_idx=batch_idx[0:N]
         np.random.shuffle(batch_idx)
         batch_list=[]
-
+    
         for i in range(0,int(num_batches)):
             idx=np.argwhere(batch_idx==i)
             batch_list.append(idx)
-
+    
         return batch_list,num_batches
     
     @jit
     def _sigmoid(self,z):
         return 1/(1+np.exp(-z))
 
-
+    @jit
     def get_pseudo_likelihood(self,x):
-        
         v=x.copy()        
         idx = (np.arange(v.shape[0]),
-               np.random.randint(0, v.shape[1], v.shape[0]))
+                np.random.randint(0, v.shape[1], v.shape[0]))
         v[idx]=1-v[idx]
     
-
+    
         N=self.vbias.shape[0]
         PL=N*np.log(self._sigmoid(self.free_energy(v)-self.free_energy(x)))
-        
-        return PL.mean()
-        
     
+        return PL.mean()
+
+    @jit
     def free_energy(self,x):
         F=-np.dot(x,self.vbias)-np.sum(np.logaddexp(0,np.dot(x,self.W)+self.hbias),axis=1)
         return F
 if __name__=="__main__":
-    rbm=binary_RBM(10,batchSize=256,alpha=1e-5,epoches=100)
-    x=np.random.rand(10000,10)
+    import matplotlib.pyplot as plt
+    
+    x=np.load('trainIm.pkl')/255.0
+    #x=x.reshape((60000,784))
+    x=x.reshape((784,60000)).T
+ 
+    
+    rbm=binary_RBM(n_visible=784,alpha=1e-5,lr=.01,epoches=10)
     rbm.fit(x)
+    
