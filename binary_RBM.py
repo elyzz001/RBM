6dad877e675c256cd224f73b045d750bc33a3135
ceroytres <dmarez8ece@gmail.com>
2016-03-24 20:06:05 -0700

    minor changes

--- binary_RBM.py
+++ binary_RBM.py
@@ -5,10 +5,12 @@ from numba import jit
 
 class binary_RBM(object):
 
-    def __init__(self,n_visible=None,n_hidden=256,batchSize=256,lr=0.1,alpha=0,mu=.95,epoches=1):
+    def __init__(self,n_visible=None,n_hidden=256,batchSize=256,lr=0.1,alpha=0,
+                 mu=.95,epochs=1,k=10):
         self.n_hidden=n_hidden
         self.n_visible=n_visible
         self.batchSize=batchSize
+        self.k=k
         self.alpha=alpha
         
         self.W=np.random.rand(n_visible,n_hidden)#np.random.randn(n_visible,n_hidden)
@@ -17,7 +19,7 @@ class binary_RBM(object):
         
         self.hbias=np.zeros(n_hidden)
         self.vbias=np.zeros(n_visible)
-        self.epoches=epoches
+        self.epochs=epochs
         
         self.lr=lr
         self.mu=mu
@@ -32,7 +34,7 @@ class binary_RBM(object):
         cost=self.get_pseudo_likelihood(x)
         print("Epoch %d Pseudo-likelihood cost:%f" % (0,cost))
     
-        for t in range(0,self.epoches):
+        for t in range(0,self.epochs):
             N=x.shape[0]
             batches,num_batches=self._batchLists(N)
             num_batches=int(num_batches)
@@ -47,11 +49,11 @@ class binary_RBM(object):
                 if t==0 and i==0:
                     h=p_h>np.random.rand(p_h.shape[0],p_h.shape[1])
                 
-            
-                p_v=self._sigmoid(np.dot(h,self.W.T)+self.vbias)
-                v=p_v>np.random.rand(p_v.shape[0],p_v.shape[1])
-                q_h=self._sigmoid(np.dot(v,self.W)+self.hbias)
-                h=q_h>np.random.rand(q_h.shape[0],q_h.shape[1])
+                for k in range(0,self.k):
+                    p_v=self._sigmoid(np.dot(h,self.W.T)+self.vbias)
+                    v=p_v>np.random.rand(p_v.shape[0],p_v.shape[1])
+                    q_h=self._sigmoid(np.dot(v,self.W)+self.hbias)
+                    h=q_h>np.random.rand(q_h.shape[0],q_h.shape[1])
                 
         
                 g_W=np.dot(data.T,p_h)-np.dot(v.T,q_h)
@@ -128,10 +130,10 @@ if __name__=="__main__":
     x=np.load('trainIm.pkl')/255.0
     x=x.reshape((784,60000)).T
     
-    rbm=binary_RBM(n_visible=784,n_hidden=256,alpha=1e-6,lr=.1,batchSize=256,epoches=20)
+    rbm=binary_RBM(n_visible=784,n_hidden=256,alpha=0,lr=.1,batchSize=20,epochs=4)
     rbm.fit(x)
-    
-    v=rbm.gibbs_sample(5000)
+    v=rbm.gibbs_sample(10000)
     plt.imshow(v.reshape((28,28)),cmap='gray')
     plt.show()
+    
     
